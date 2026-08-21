60bea8bf3c9b14c8a307d03780b0ce526a402502
thetechmango <erikesko2@gmail.com>
2026-08-21 14:36:05 -0400

    32 colors

--- mango-grid/main.js
+++ mango-grid/main.js
@@ -37,9 +37,9 @@ let gridView = new Uint8Array(gridBuffer);
 const colors = [
     "#ffffff", "#dddddd", "#aaaaaa", "#666666", "#222222", "#000000",
     "#ff0000", "#700000", "#ff6000", "#ff9000", "#ffff00",
-    "#ffff80", "#00ff00", "#007000", "#a0f0a0", "#00ffb0",
+    "#ffff80", "#b0ff00", "#00ff00", "#007000", "#a0f0a0", "#00ffb0",
     "#00ffff", "#00a0ff", "#acdbff", "#0000ff", "#000070",
-    "#8000d0", "#cd97ef", "#ff00ff",
+    "#7000ff", "#8000d0", "#cd97ef", "#ff00ff", "#ff0080",
     "#f7e1c5", "#caaa81", "#98713e", "#5b390e", "#492403"
 ];
 
